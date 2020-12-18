5b09f7a16819690cdaddfccf4840e37e33274835
Qifan Deng <plocircle@live.com>
2020-12-18 12:09:25 +0800

    New: Init Worker and Master

---
+++ containers/newWorker/woker.py
@@ -0,0 +1,19 @@
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://127.0.0.1:5000')
+sio.wait()
