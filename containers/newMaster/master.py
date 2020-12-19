ce59245da93947385fbd03f6f2185a2911c1f4cf
Qifan Deng <plocircle@live.com>
2020-12-19 09:38:29 +0800

    New: backup

--- containers/newMaster/master.py
+++ containers/newMaster/master.py
@@ -1,28 +1,46 @@
 import eventlet
 import socketio
+import logging
 
-sio = socketio.Server()
-app = socketio.WSGIApp(sio, static_files={
-    '/': {'content_type': 'text/html', 'filename': 'html/index.html'}
-})
+from logger import get_logger
+from registry import Registry
+from datatype import Master, NodeSpecs
 
+class RegistryNamespace(socketio.Namespace):
 
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
+    def __init__(self, namespace=None, logger=logging):
+        super(RegistryNamespace, self).__init__(namespace=namespace)
+        self.registry = Registry()
+        self.logger = logger
 
+    def connect(self, socketID, environ):
+        print("connect")
+        pass
 
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
+    def register(self, socketID, t):
+        
+        workerID = self.registry.addWoker(socketID, NodeSpecs(1, 2, 3, 4))
+        sio.emit("id",  to=socketID, data=workerID, )
+        logger.info(self.registry.workers)
 
+    def my_message(self, socketID, data):
+        print('message ', data)
 
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
+    def disconnect(self, socketID):
+        print('disconnect ', socketID)
 
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)),
-                         app, log=None, log_output=False)
-                         
+    logger = get_logger('Master', logging.INFO)
+
+    sio = socketio.Server()
+    app = socketio.WSGIApp(sio, static_files={
+        '/': {'content_type': 'text/html', 'filename': 'html/index.html'}
+    })
+    sio.register_namespace(RegistryNamespace('/registry', logger))
+
+    host = ''
+    port = 5000
+    logger.info("[*] Master serves at: %s:%d" % (host, port))
+    eventlet.wsgi.server(eventlet.listen((host, port)),
+                         app, log_output=False)
--- containers/newWorker/woker.py
+++ containers/newWorker/woker.py
@@ -1,19 +1,31 @@
 import socketio
+from datatype import NodeSpecs
 
 sio = socketio.Client()
 
-@sio.event
+
+@sio.event(namespace='/registry')
 def connect():
     print('connection established')
 
-@sio.event
+
+@sio.event(namespace='/registry')
+def id(data):
+    print('registered', data)
+
+
+@sio.event(namespace='/registry')
 def my_message(data):
     print('message received with ', data)
     sio.emit('my response', {'response': 'my response'})
 
-@sio.event
+
+@sio.event(namespace='/registry')
 def disconnect():
     print('disconnected from server')
 
-sio.connect('http://127.0.0.1:5000')
+
+sio.connect('http://127.0.0.1:5000', namespaces=['/registry'])
+print(sio.connection_namespaces)
+sio.emit('register', 1, namespace='/registry')
 sio.wait()
