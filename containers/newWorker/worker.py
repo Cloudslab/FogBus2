d201a0a0dbd3fcb661462c2c37284f1d9e358cb8
Qifan Deng <plocircle@live.com>
2021-01-05 11:36:17 +0800

    Refactor Worker

--- containers/newWorker/worker.py
+++ containers/newWorker/worker.py
@@ -1,12 +1,14 @@
 import logging
 from apps import *
-from broker import Broker
+from datatype import Broker
 
 if __name__ == '__main__':
     apps = [TestApp(0), FaceDetection(1), EyeDetection(2), ColorTracking(3)]
     broker = Broker(
-        host='127.0.0.1',
-        port=5000,
+        masterIP='127.0.0.1',
+        masterPort=5000,
+        thisIP='127.0.0.1',
+        thisPort=6000,
         appIDs=apps,
         logLevel=logging.DEBUG)
     broker.run()
