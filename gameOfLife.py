99be8c844c1dbf05dac0188fedfa3ada337b4770
Qifan Deng <plocircle@live.com>
2021-01-26 23:29:57 +0800

    fix bugs

--- gameOfLife.py
+++ gameOfLife.py
@@ -9,16 +9,14 @@ class GameOfLife:
             height=512,
             width=1024,
             generationNumber=None,
-            fps=15,
+            fps=60,
             resizeFactor=8):
         self.__resizeFactor = resizeFactor
         self.height = height // self.__resizeFactor
         self.width = width // self.__resizeFactor
         self.generationNumber = generationNumber
-        self.boundaries = [[1, 1], [self.height + 1, self.width + 1]]
-        self.world = 0 * (np.random.rand(
-            self.boundaries[1][0] + 2,
-            self.boundaries[1][1] + 2) * 2).astype('uint8')
+        self.boundaries = [[0, 0], [self.height, self.width]]
+        self.world = np.zeros((self.height, self.width, 1), np.uint8)
         self.newStates = []
         self.frameUpdateGap = 1 / fps
 
@@ -33,10 +31,35 @@ class GameOfLife:
             points.append((i + 4, j + k))
         return points
 
+    def startWithText(self, text):
+        position = (self.height // 4, self.width // 4)
+        fontScale = 1
+        color = (255, 0, 0)
+        thickness = 1
+        world = cv2.putText(
+            self.world,
+            text,
+            position,
+            0,
+            fontScale,
+            color,
+            thickness,
+            cv2.LINE_AA)
+        _, self.world = cv2.threshold(world, 127, 255, cv2.THRESH_BINARY)
+
+        cv2.imshow(
+            'Game of Life',
+            cv2.resize(
+                self.world,
+                (self.width * self.__resizeFactor,
+                 self.height * self.__resizeFactor),
+                interpolation=cv2.INTER_AREA))
+        cv2.waitKey(0)
+
     def setPoints(self, points):
         self.boundaries = [list(points[0]), list(points[0])]
         for i, j in points:
-            self.world[i][j] = 255
+            self.world[i % self.height][j % self.width] = 255
             self.updateBoundaries(i, j)
 
     def updateBoundaries(self, i, j):
@@ -44,9 +67,9 @@ class GameOfLife:
         if i <= self.boundaries[0][0]:
             self.boundaries[0][0] = i - 1
         elif i >= self.boundaries[1][0]:
-            self.boundaries[1][0] = i + 1
+            self.boundaries[1][0] = (i + 1) % self.width
         if j <= self.boundaries[0][1]:
-            self.boundaries[0][1] = j - 1
+            self.boundaries[0][1] = (j - 1) % self.height
         elif j >= self.boundaries[1][1]:
             self.boundaries[1][1] = j + 1
 
@@ -77,18 +100,18 @@ class GameOfLife:
                 break
 
     def move(self):
-        for i in range(self.boundaries[0][0], self.boundaries[1][0] + 1):
-            for j in range(self.boundaries[0][1], self.boundaries[1][1] + 1):
+        for i in range(0, self.height):
+            for j in range(0, self.width):
                 if self.doesChange(self.world, i, j):
                     self.newStates.append((i, j))
                     self.updateBoundaries(i, j)
 
     def changeStates(self):
         for i, j in self.newStates:
-            if self.world[i][j] == 0:
-                self.world[i][j] = 255
+            if self.world[i % self.height][j % self.width] == 0:
+                self.world[i % self.height][j % self.width] = 255
             else:
-                self.world[i][j] = 0
+                self.world[i % self.height][j % self.width] = 0
         self.newStates = []
 
     def doesChange(self, world, i, j) -> bool:
@@ -96,18 +119,18 @@ class GameOfLife:
         if count == 2:
             return False
         if count == 3:
-            return not world[i][j] == 255
-        return not world[i][j] == 0
+            return not world[i % self.height][j % self.width] == 255
+        return not world[i % self.height][j % self.width] == 0
 
     def neighboursCount(self, world, i, j):
         count = 0
         count += 1 if world[i - 1][j - 1] else 0
-        count += 1 if world[i - 1][j] else 0
+        count += 1 if world[i - 1][j % self.width] else 0
         count += 1 if world[i - 1][(j + 1) % self.width] else 0
-        count += 1 if world[i][j - 1] else 0
-        count += 1 if world[i][(j + 1) % self.width] else 0
+        count += 1 if world[i % self.height][j - 1] else 0
+        count += 1 if world[i % self.height][(j + 1) % self.width] else 0
         count += 1 if world[(i + 1) % self.height][j - 1] else 0
-        count += 1 if world[(i + 1) % self.height][j] else 0
+        count += 1 if world[(i + 1) % self.height][j % self.width] else 0
         count += 1 if world[(i + 1) % self.height][(j + 1) % self.width] else 0
         return count
 
@@ -116,5 +139,6 @@ if __name__ == '__main__':
     game = GameOfLife(
         height=1024,
         width=2048)
-    game.setPoints(game.examplePoints())
+    game.startWithText('Qifan Deng')
+    # game.setPoints(game.examplePoints())
     game.run()
