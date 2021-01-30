0f1c28bcc76da21cf99d4dff358e1fdfa91977fb
Qifan Deng <plocircle@live.com>
2021-01-30 13:13:06 +0800

    build simultaneously

--- containers/rebuild.sh
+++ containers/rebuild.sh
@@ -1,15 +1,15 @@
 
 build(){
-docker-compose \
-    build \
-    --build-arg  HTTP_PROXY="http://172.17.0.1:10801" \
-    --build-arg  HTTPS_PROXY="http://172.17.0.1:10801" \
-
+  cd $1
+  docker-compose \
+      build \
+      --build-arg  HTTP_PROXY="http://172.17.0.1:10801" \
+      --build-arg  HTTPS_PROXY="http://172.17.0.1:10801" \
 }
 
-cd newLogger && build &
-cd  ../newMaster && build &
-cd  ../newUser && build &
-cd  ../newWorker && build &
+cd  build newLogger &
+cd  build  newMaster &
+cd  build newUser &
+cd  build  newWorker &
 
 cd sources/tasks && ./rebuild.sh
