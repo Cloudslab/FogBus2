1a6cc5f2421d31ba011d43f649dbedace20d32a7
Qifan Deng <plocircle@live.com>
2021-02-05 11:53:12 +0800

    improve graph

--- containers/graph.py
+++ containers/graph.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import numpy as np
 
@@ -40,31 +41,53 @@ class Graph:
             self.data[algorithmName] = np.vstack((self.data[algorithmName], np.asarray(data)))
 
     def draw(self, statMethod: str):
+        if not len(self.data):
+            return
+        roundNum = 0
+        rangeNum = 0
+        for data in self.data.values():
+            if len(data.shape) == 1:
+                roundNum = 1
+                rangeNum = data.shape[0]
+            else:
+                roundNum = data.shape[0]
+                rangeNum = data.shape[1]
+            break
+        if roundNum == 0:
+            return
+
         fig, ax = plt.subplots()
         # fig.ylabel('Respond Time (ms)')
-        x = [i for i in range(1, 61)]
+        x = [i for i in range(1, rangeNum + 1)]
         algorithms = []
         for algorithmName, data in self.data.items():
-            if statMethod == 'mean':
-                data = np.mean(self.data[algorithmName], axis=0)
+            if len(self.data[algorithmName].shape) == 1:
+                data = self.data[algorithmName]
             else:
-                data = np.median(self.data[algorithmName], axis=0)
+                if statMethod == 'mean':
+                    data = np.mean(self.data[algorithmName], axis=0)
+                else:
+                    data = np.median(self.data[algorithmName], axis=0)
             ax.plot(x, data)
             algorithms.append(algorithmName)
         ax.legend(algorithms)
         ax.set_ylabel('Respond Time (ms)')
         ax.set_xlabel('Iteration Number')
         statMethod = statMethod[0].upper() + statMethod[1:]
+
         ax.set_title(
-            '%s Delays of 10 Rounds' %
-            statMethod
+            '%s Delays of %d Rounds' %
+            (statMethod, roundNum)
         )
         plt.show()
 
 
 if __name__ == '__main__':
+    resultPath = './'
+    if len(sys.argv) > 1:
+        resultPath = sys.argv[1]
     graph_ = Graph(
-        './',
+        resultPath,
         ['NSGA2', 'NSGA3']
     )
     graph_.run()
