ddc93ed0d92ef3d3942b9605b46b549d8f4e9e37
Qifan Deng <plocircle@live.com>
2021-03-21 01:27:18 +0800

    NetProfile: fix

--- containers/newMaster/sources/master.py
+++ containers/newMaster/sources/master.py
@@ -234,7 +234,7 @@ class Master(Registry):
         self.medianProcessTime = {**self.medianProcessTime, **profilers[3]}
         self.medianRespondTime = {**self.medianRespondTime, **profilers[4]}
         self.imagesAndRunningContainers = {**self.imagesAndRunningContainers, **profilers[5]}
-        self.bps = {**self.imagesAndRunningContainers, **profilers[6]}
+        self.bps = {**self.bps, **profilers[6]}
 
         # update
         self.scheduler.medianPackageSize = self.medianPackageSize
